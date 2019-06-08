3eb832569feded2bb8e43f25b96fc023882fea69
Karthick Ramakrishnan <magnumnighthhawk@gmail.com>
2019-06-08 12:26:46 +0530

    React router code update

--- router-example/src/App.js
+++ router-example/src/App.js
@@ -1,85 +1,71 @@
-import React from 'react';
-import './App.css';
-import {BrowserRouter as Router,Route} from 'react-router-dom'
-import { createBrowserHistory } from 'history';
+import React from "react";
+import "./App.css";
+import { BrowserRouter as Router, Route } from "react-router-dom";
 
-export const history = createBrowserHistory();
-
-class User extends React.Component{
-  render(){
-  return(
-
-    <h1>  Welcome User </h1>
-
-  );
-
-}
+class User extends React.Component {
+  render() {
+    return <h1> Welcome User <b>{this.props.match.params.username}</b> </h1>;
+  }
 }
 
-class App extends React.Component{
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      username: "",
+    };
+    this.updateChange = this.updateChange.bind(this);
+    this.handleStart = this.handleStart.bind(this);
+  }
 
-  constructor(props){
-    super(props)
-    this.state ={
-      username:''
+  updateChange = event => {
+    this.setState({
+      username: event.target.value
+    });
+  };
+
+  handleStart = (props) => {
+    if (this.state.username.length > 0) {
+      props.history.push(`/user/${this.state.username}`);
     }
-    this.updateChange = this.updateChange.bind(this)
-    this.handleStart = this.handleStart.bind(this)
-  }
+  };
 
-updateChange = (event) => {
-this.setState({
-  username : event.target.value
-  })
-  }
-
-  handleStart = () => {
-
-    history.push(`/user/${this.state.username}`)
-  }
-
-  render(){
-    return(
-
+  render() {
+    return (
       <Router>
-
-      <div className="box">
-
-            <div className="user">
-
-
-                <Route path="/" exact render={
-                     ()=> {return(
+        <div className="box">
+          <div className="user">
+            <Route
+              path="/"
+              exact
+              render={(props) => {
+                return (
                   <div>
-                      <h1> Welcome </h1>
-                      <input type="text"
-                        id="username"
-                        placeholder="username"
-                        value={this.state.username}
-                        onChange={this.updateChange} />
+                    <h1> Welcome </h1>
+                    <input
+                      type="text"
+                      id="username"
+                      placeholder="username"
+                      value={this.state.username}
+                      onChange={this.updateChange}
+                    />
 
-                     <input type="submit"
-                            value="Start"
-                            onClick={this.handleStart}/>
+                    <input
+                      type="submit"
+                      value="Start"
+                      onClick={() => this.handleStart(props)}
+                    />
                   </div>
-                    );
-                  }
-                  }/>
+                );
+              }}
+            />
 
-                  <Route path="/user/:username" exact component={User}/>
-
-
-
-              </div>
-
-      </div>
+            <Route path="/user/:username" exact component={User} />
+          </div>
+        </div>
       </Router>
-
-
     );
-
+  }
 }
-}
-
 
 export default App;
